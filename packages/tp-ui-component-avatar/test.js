1377bc6d23590b3f5395bf50ac01ab468ebaf60f
Tom Genoni <tom@thumbtack.com>
2019-01-31 18:21:58 -0800

    Move scss packages into thumbprint-scss

--- packages/tp-ui-component-avatar/test.js
+++
@@ -1,8 +0,0 @@
-const sass = require('node-sass');
-
-it('compiles correctly', () => {
-    const css = sass
-        .renderSync({ file: 'packages/tp-ui-component-avatar/_index.scss' })
-        .css.toString();
-    expect(css).toMatchSnapshot();
-});
--- packages/tp-ui-component-loader/test.js
+++
@@ -1,8 +0,0 @@
-const sass = require('node-sass');
-
-it('compiles correctly', () => {
-    const css = sass
-        .renderSync({ file: 'packages/tp-ui-component-loader/_index.scss' })
-        .css.toString();
-    expect(css).toMatchSnapshot();
-});
