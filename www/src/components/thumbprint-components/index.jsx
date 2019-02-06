2fd519d3ae44434a8149c49fce73df332f1e2d47
Daniel O'Connor <doconnor@thumbtack.com>
2019-02-06 11:53:37 -0800

    Fix docs

--- www/src/components/thumbprint-components/index.jsx
+++ www/src/components/thumbprint-components/index.jsx
@@ -3,31 +3,7 @@ import ComponentHeader from './component-header';
 import ComponentFooter from './component-footer';
 import DeprecatedComponentAlert from './deprecated-component-alert';
 
-import '@thumbtack/tp-ui-component-alert';
-import '@thumbtack/tp-ui-component-avatar';
-import '@thumbtack/tp-ui-component-form-note';
-import '@thumbtack/tp-ui-component-loader';
-import '@thumbtack/tp-ui-element-button';
-import '@thumbtack/tp-ui-element-checkbox';
-import '@thumbtack/tp-ui-element-fieldset';
-import '@thumbtack/tp-ui-element-hr';
-import '@thumbtack/tp-ui-element-img';
-import '@thumbtack/tp-ui-element-input';
-import '@thumbtack/tp-ui-element-label';
-import '@thumbtack/tp-ui-element-link';
-import '@thumbtack/tp-ui-element-list';
-import '@thumbtack/tp-ui-element-radio';
-import '@thumbtack/tp-ui-element-select';
-import '@thumbtack/tp-ui-element-table';
-import '@thumbtack/tp-ui-element-textarea';
-import '@thumbtack/tp-ui-element-type';
-import '@thumbtack/tp-ui-layout-block-list';
-import '@thumbtack/tp-ui-layout-button-row';
-import '@thumbtack/tp-ui-layout-form';
-import '@thumbtack/tp-ui-layout-grid';
-import '@thumbtack/tp-ui-layout-input-row';
-import '@thumbtack/tp-ui-layout-longread';
-import '@thumbtack/tp-ui-layout-wrap';
+import '@thumbtack/thumbprint-scss';
 
 export { ComponentHeader, ComponentFooter, DeprecatedComponentAlert };
 
