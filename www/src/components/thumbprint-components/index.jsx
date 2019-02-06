a6503b3b17072719d35ff62d80a78869ea67f304
Daniel O'Connor <doconnor@thumbtack.com>
2019-02-05 17:32:26 -0800

    Use `www/src/pages` for Thumbprint Components

--- www/src/components/thumbprint-components/index.jsx
+++ www/src/components/thumbprint-components/index.jsx
@@ -8,7 +8,32 @@ import PropType from './prop-type';
 import PlatformNav from './platform-nav';
 import ComponentPackageTable from './component-package-table';
 import { MDXRenderer, InlineCode, H2, H3 } from '../mdx';
-import '@thumbtack/thumbprint-scss';
+
+import '@thumbtack/tp-ui-component-alert';
+import '@thumbtack/tp-ui-component-avatar';
+import '@thumbtack/tp-ui-component-form-note';
+import '@thumbtack/tp-ui-component-loader';
+import '@thumbtack/tp-ui-element-button';
+import '@thumbtack/tp-ui-element-checkbox';
+import '@thumbtack/tp-ui-element-fieldset';
+import '@thumbtack/tp-ui-element-hr';
+import '@thumbtack/tp-ui-element-img';
+import '@thumbtack/tp-ui-element-input';
+import '@thumbtack/tp-ui-element-label';
+import '@thumbtack/tp-ui-element-link';
+import '@thumbtack/tp-ui-element-list';
+import '@thumbtack/tp-ui-element-radio';
+import '@thumbtack/tp-ui-element-select';
+import '@thumbtack/tp-ui-element-table';
+import '@thumbtack/tp-ui-element-textarea';
+import '@thumbtack/tp-ui-element-type';
+import '@thumbtack/tp-ui-layout-block-list';
+import '@thumbtack/tp-ui-layout-button-row';
+import '@thumbtack/tp-ui-layout-form';
+import '@thumbtack/tp-ui-layout-grid';
+import '@thumbtack/tp-ui-layout-input-row';
+import '@thumbtack/tp-ui-layout-longread';
+import '@thumbtack/tp-ui-layout-wrap';
 
 const ComponentHeader = ({ data, deprecated }) => {
     const { platformNav: platformNavQueryResults, packageTable, reactComponentProps } = data;
