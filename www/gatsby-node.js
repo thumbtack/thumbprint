7fbc443cac63378db01be2e5b5078b84ef14538b
Daniel O'Connor <doconnor@thumbtack.com>
2019-02-14 16:19:02 -0800

    Fix file watching/reloading for components
    This fixes #122.

--- www/gatsby-node.js
+++ www/gatsby-node.js
@@ -21,6 +21,20 @@ exports.onCreateWebpackConfig = ({ actions }) => {
     actions.setWebpackConfig({
         resolve: {
             modules: [path.resolve(__dirname, 'src'), 'node_modules'],
+            alias: {
+                // Use Thumbprint React and SCSS source files when `import`'ing them in the
+                // documentation. By default, Webpack will use the value of the `main` or `module`
+                // field in the package's `package.json` file. This makes it difficult to develop
+                // locally because it'd require running re-compiling the code after each change.
+                '@thumbtack/thumbprint-react':
+                    process.env.NODE_ENV !== 'production'
+                        ? path.resolve(__dirname, '../packages/thumbprint-react/index.js')
+                        : '@thumbtack/thumbprint-react',
+                '@thumbtack/thumbprint-scss':
+                    process.env.NODE_ENV !== 'production'
+                        ? path.resolve(__dirname, '../packages/thumbprint-scss/components.scss')
+                        : '@thumbtack/thumbprint-scss',
+            },
         },
     });
 };
