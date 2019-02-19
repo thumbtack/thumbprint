81d990a1fe4fea299a29fb74f779b9c6a6a0f0e0
Daniel O’Connor <danielcarraraoconnor@gmail.com>
2019-02-19 15:27:41 -0800

    Remove Babel SVG plugin (#128)
    This PR removes the need for the `babel-plugin-inline-react-svg` plugin. The plugin was previously used to transform SVG imports into React components that could be used on a page.
    
    There are a few benefits to removing this:
    
    1. The babel plugin (or Gatsby's babel setup?) is caching SVGs too aggressively. Changes to SVGs are not appearing until we restarted Gatsby. This caused a bug two weeks ago when I minified the star in the `StarRating` component and didn't realize that I had broken it.
    2. This means we now have no custom babel plugins. Simplifies config and makes it easier for developers to work in the Thumbprint codebase.
    3. Importing images in Gatsby will now be consistent across SVGs and non-SVGs.
    
    There are two types of JSX files that were changed:
    
    * `packages/**/*.jsx`: I've converted the SVGs into tiny React components and minimized the SVG.
    * `www/**/*.jsx`: Gatsby supports importing SVGs out-of-the-box. The imports now return a URL instead of a React component.
    
    This modifies three React components: `StarRating`, `ModalStandard`, and `Avatar`. I've updated snapshots and manually verified that they are rendering correctly.

--- www/gatsby-node.js
+++ www/gatsby-node.js
@@ -1,22 +1,5 @@
 const path = require('path');
 
-exports.onCreateBabelConfig = ({ actions }) => {
-    actions.setBabelPlugin({
-        name: 'babel-plugin-inline-react-svg',
-        options: {
-            svgo: {
-                plugins: [
-                    {
-                        removeAttrs: {
-                            attrs: '(data-name)',
-                        },
-                    },
-                ],
-            },
-        },
-    });
-};
-
 exports.onCreateWebpackConfig = ({ actions }) => {
     actions.setWebpackConfig({
         resolve: {
--- www/src/components/thumbprint-www/hero/index.jsx
+++ www/src/components/thumbprint-www/hero/index.jsx
@@ -1,17 +1,17 @@
 import React from 'react';
 import styles from './index.module.scss';
-import IntroImg from './heroImg.svg';
-import ThumbprintLogo from '../../container/thumbprintLogo.svg';
+import heroImg from './heroImg.svg';
+import thumbprintLogo from '../../container/thumbprintLogo.svg';
 
 const description = 'Assets for building high-quality, consistent user experiences at Thumbtack.';
 
 const Hero = () => (
     <div className={styles.hero}>
         <div className={styles.imageWrap}>
-            <IntroImg className={styles.image} />
+            <img src={heroImg} className={styles.image} alt="" />
         </div>
         <div className={styles.textBlock}>
-            <ThumbprintLogo className={styles.logo} />
+            <img src={thumbprintLogo} className={styles.logo} alt="Thumbprint" />
             <div className={styles.subhead}>Design System</div>
             <div className={styles.description}>{description}</div>
         </div>
