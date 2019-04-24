171804b07999c52247ed1f51250e3b562786537c
Daniel O'Connor <doconnor@thumbtack.com>
2019-04-24 10:51:23 -0700

    Improve component design.

--- www/src/components/thumbprint-components/overview/index.jsx
+++ www/src/components/thumbprint-components/overview/index.jsx
@@ -2,6 +2,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import classNames from 'classnames';
+import { Text } from '@thumbtack/thumbprint-react';
 import styles from './index.module.scss';
 
 export const Dot = ({ status }) => {
@@ -23,76 +24,87 @@ export const Dot = ({ status }) => {
                     [styles.dotToDo]: status === 'to-do' || status === 'n/a',
                 })}
             />
-            {/* <Text size={2}>{statusMap[status]}</Text> */}
         </>
     );
 };
 
 export const ComponentRow = ({ name, usage, react, scss, ios, android, note }) => (
     <>
-        <tr className="bt b-gray-300 tl">
-            <th rowSpan={5} className="pr2 pv2">
+        <tr className="bt b-gray-300 bw-2 tl">
+            <th rowSpan={5} className="pr2 pv2 v-top">
                 {name}
             </th>
-            <th className="normal bl b-gray-300 ph2 pv1">Usage</th>
-            <td className="bl b-gray-300 pa2 tc bw-2">
+            <th className="normal ph2 pv1">
+                <Text size={2}>Usage</Text>
+            </th>
+            <td className="pa2 tc bw-2">
                 <Dot status={usage.design} />
             </td>
-            <td className="bl b-gray-300 pa2 tc">
+            <td className="pa2 tc">
                 <Dot status={usage.development} />
             </td>
-            <td className="bl b-gray-300 pa2 tc">
+            <td className="pa2 tc">
                 <Dot status={usage.documentation} />
             </td>
-            <td rowSpan={5} className="bl b-gray-300 pa2">
-                {note}
+            <td rowSpan={5} className="pa2 v-top black-300">
+                <Text size={2} className="mw6">
+                    {note}
+                </Text>
             </td>
         </tr>
         <tr className="bt b-gray-300 tl">
-            <th className="normal bl b-gray-300 ph2 pv1">React</th>
-            <td className="bl b-gray-300 pa2 tc bw-2">
+            <th className="normal ph2 pv1">
+                <Text size={2}>React</Text>
+            </th>
+            <td className="pa2 tc bw-2">
                 <Dot status={react.design} />
             </td>
-            <td className="bl b-gray-300 pa2 tc">
+            <td className="pa2 tc">
                 <Dot status={react.development} />
             </td>
-            <td className="bl b-gray-300 pa2 tc">
+            <td className="pa2 tc">
                 <Dot status={react.documentation} />
             </td>
         </tr>
         <tr className="bt b-gray-300 tl">
-            <th className="normal bl b-gray-300 ph2 pv1">SCSS</th>
-            <td className="bl b-gray-300 pa2 tc bw-2">
+            <th className="normal ph2 pv1">
+                <Text size={2}>SCSS</Text>
+            </th>
+            <td className="pa2 tc bw-2">
                 <Dot status={scss.design} />
             </td>
-            <td className="bl b-gray-300 pa2 tc">
+            <td className="pa2 tc">
                 <Dot status={scss.development} />
             </td>
-            <td className="bl b-gray-300 pa2 tc">
+            <td className="pa2 tc">
                 <Dot status={scss.documentation} />
             </td>
         </tr>
         <tr className="bt b-gray-300 tl">
-            <th className="normal bl b-gray-300 ph2 pv1">iOS</th>
-            <td className="bl b-gray-300 pa2 tc bw-2">
+            <th className="normal ph2 pv1">
+                <Text size={2}>iOS</Text>
+            </th>
+            <td className="pa2 tc bw-2">
                 <Dot status={ios.design} />
             </td>
-            <td className="bl b-gray-300 pa2 tc">
+            <td className="pa2 tc">
                 <Dot status={ios.development} />
             </td>
-            <td className="bl b-gray-300 pa2 tc">
+            <td className="pa2 tc">
                 <Dot status={ios.documentation} />
             </td>
         </tr>
         <tr className="bt b-gray-300 tl">
-            <th className="normal bl b-gray-300 ph2 pv1">Android</th>
-            <td className="bl b-gray-300 pa2 tc bw-2">
+            <th className="normal ph2 pv1">
+                <Text size={2}>Android</Text>
+            </th>
+            <td className="pa2 tc bw-2">
                 <Dot status={android.design} />
             </td>
-            <td className="bl b-gray-300 pa2 tc">
+            <td className="pa2 tc">
                 <Dot status={android.development} />
             </td>
-            <td className="bl b-gray-300 pa2 tc">
+            <td className="pa2 tc">
                 <Dot status={android.documentation} />
             </td>
         </tr>
