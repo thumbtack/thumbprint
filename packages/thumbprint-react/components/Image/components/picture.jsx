3537a4218cc636bd5bb370c6ab62a981f0d03c1a
Daniel O'Connor <doconnor@thumbtack.com>
2019-05-29 14:35:28 -0700

    Fix safari-triple loading bug.

--- packages/thumbprint-react/components/Image/components/picture.jsx
+++ packages/thumbprint-react/components/Image/components/picture.jsx
@@ -1,32 +1,37 @@
-import React, { forwardRef, useState } from 'react';
+import React, { useState } from 'react';
 import styles from './picture.module.scss';
 
-const Picture = forwardRef((props, ref) => {
+const Picture = props => {
     const { src, sources, sizes, alt, style, ...rest } = props;
 
     const [isLoaded, setIsLoaded] = useState(false);
+    const isResponsive = sources && sources.length > 0;
 
-    return (
-        <picture>
-            {sources.map(({ type, srcSet }) => (
-                <source type={type} srcSet={srcSet} key={type} sizes={sizes} />
-            ))}
-            <img
-                src={src}
-                alt={alt}
-                style={style}
-                ref={ref}
-                {...rest}
-                onLoad={() => {
-                    setIsLoaded(true);
-                }}
-                className={isLoaded ? styles.imageLoaded : styles.imageLoading}
-            />
-        </picture>
+    const img = (
+        <img
+            src={src}
+            alt={alt}
+            style={style}
+            {...rest}
+            onLoad={() => {
+                setIsLoaded(true);
+            }}
+            className={isLoaded ? styles.imageLoaded : styles.imageLoading}
+        />
     );
-});
 
-// Needed because of the `forwardRef`.
-Picture.displayName = 'Picture';
+    if (isResponsive) {
+        return (
+            <picture>
+                {sources.map(({ type, srcSet }) => (
+                    <source type={type} srcSet={srcSet} key={type} sizes={sizes} />
+                ))}
+                {img}
+            </picture>
+        );
+    }
+
+    return img;
+};
 
 export default Picture;
