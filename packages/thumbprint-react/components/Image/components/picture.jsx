67533b9d08f4cfcc6d7185b67c4001e2ada5ad48
Daniel O'Connor <doconnor@thumbtack.com>
2019-05-31 10:26:16 -0700

    Work around the Safari bug.

--- packages/thumbprint-react/components/Image/components/picture.jsx
+++ packages/thumbprint-react/components/Image/components/picture.jsx
@@ -4,34 +4,36 @@ import styles from './picture.module.scss';
 const Picture = props => {
     const { src, sources, sizes, alt, style, ...rest } = props;
 
+    // We separate `webp` from the `jpeg`/`png` so that we can apply the `imgTagSource` directly
+    // onto the `img` tag. While this makes the code messier, it is needed to work around a bug in
+    // Safari.
+    const webpSource = sources.find(s => s.type === 'image/webp');
+    const imgTagSource = sources.find(s => s.type === 'image/jpeg' || s.type === 'image/png');
+
     const [isLoaded, setIsLoaded] = useState(false);
-    const isResponsive = sources && sources.length > 0;
 
-    const img = (
-        <img
-            src={src}
-            alt={alt}
-            style={style}
-            {...rest}
-            onLoad={() => {
-                setIsLoaded(true);
-            }}
-            className={isLoaded ? styles.imageLoaded : styles.imageLoading}
-        />
+    return (
+        <picture>
+            {webpSource && (
+                <source type={webpSource.type} srcSet={webpSource.srcSet} sizes={sizes} />
+            )}
+            <img
+                // The order of `sizes`, `srcSet`, and `src` is important to work around a bug in
+                // Safari. Once the bug is fixed, we should simplify this by using `src` on the
+                // `img` tag and using `source` tags.
+                sizes={sizes}
+                srcSet={imgTagSource && imgTagSource.srcSet}
+                src={src}
+                alt={alt}
+                style={style}
+                {...rest}
+                onLoad={() => {
+                    setIsLoaded(true);
+                }}
+                className={isLoaded ? styles.imageLoaded : styles.imageLoading}
+            />
+        </picture>
     );
-
-    if (isResponsive) {
-        return (
-            <picture>
-                {sources.map(({ type, srcSet }) => (
-                    <source type={type} srcSet={srcSet} key={type} sizes={sizes} />
-                ))}
-                {img}
-            </picture>
-        );
-    }
-
-    return img;
 };
 
 export default Picture;
