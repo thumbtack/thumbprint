765c8f1fb06768135f87b310a2a81f739320d5e2
Tom Genoni <tom@thumbtack.com>
2019-06-11 13:13:30 -0700

    Update packages/thumbprint-react/components/Image/index.jsx
    Co-Authored-By: Daniel O’Connor <daniel@danoc.me>

--- packages/thumbprint-react/components/Image/index.jsx
+++ packages/thumbprint-react/components/Image/index.jsx
@@ -12,7 +12,7 @@ import styles from './index.module.scss';
 // Steps in rendering Image
 //
 // 1. Picture is rendered without src, srcSets, and with a padding-top placholder on the <img>
-// based on the aspectRatio or containerAspectRatio.
+// based on the containerAspectRatio.
 // 2. The "sizes" attr is calculated on initial render to determine width of image.
 // 3. When lazyload is triggered the src and scrSet props are populated based on the sizes value.
 // 4. The image onLoad event removes padding-top placholder and fades in the image.
