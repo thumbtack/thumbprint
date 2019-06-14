70ec6a44c1fa99dc3ad8e56cab837c8eb5727f0a
Tom Genoni <tom@thumbtack.com>
2019-06-14 15:15:47 -0700

    Image refactor to use img as placeholder (#301)
    * Refactor image to move placholder to image
    
    * Fixing object fit
    
    * Commenting
    
    * Add readme
    
    * Update comment
    
    * Refactor image to move placholder to image
    
    * Fixing object fit
    
    * Commenting
    
    * Add readme
    
    * Update comment
    
    * Update test
    
    * Update packages/thumbprint-react/components/Image/index.jsx
    
    Co-Authored-By: Daniel O’Connor <daniel@danoc.me>
    
    * Update packages/thumbprint-react/components/Image/index.jsx
    
    Co-Authored-By: Daniel O’Connor <daniel@danoc.me>
    
    * Update packages/thumbprint-react/components/Image/index.jsx
    
    Co-Authored-By: Daniel O’Connor <daniel@danoc.me>
    
    * Update comment
    
    * containerAspectRatio changes
    
    * Pass in style
    
    * Refactor image to move placholder to image
    
    * Fixing object fit
    
    * Commenting
    
    * Add readme
    
    * Update comment
    
    * Refactor image to move placholder to image
    
    * Commenting
    
    * Add readme
    
    * Update comment
    
    * Update test
    
    * Update packages/thumbprint-react/components/Image/index.jsx
    
    Co-Authored-By: Daniel O’Connor <daniel@danoc.me>
    
    * Update packages/thumbprint-react/components/Image/index.jsx
    
    Co-Authored-By: Daniel O’Connor <daniel@danoc.me>
    
    * Update packages/thumbprint-react/components/Image/index.jsx
    
    Co-Authored-By: Daniel O’Connor <daniel@danoc.me>
    
    * Update comment
    
    * containerAspectRatio changes
    
    * Pass in style
    
    * Update Image, Service card examples
    
    * Prettier
    
    * Hide flash of alt text
    
    * Overflow not needed, correcting aspect ratio
    
    * Update packages/thumbprint-react/components/Image/index.jsx
    
    Co-Authored-By: Daniel O’Connor <daniel@danoc.me>
    
    * Update packages/thumbprint-react/CHANGELOG.md
    
    Co-Authored-By: Daniel O’Connor <daniel@danoc.me>
    
    * Update packages/thumbprint-react/components/Image/index.jsx
    
    Co-Authored-By: Daniel O’Connor <daniel@danoc.me>
    
    * Update packages/thumbprint-react/components/Image/index.jsx
    
    Co-Authored-By: Daniel O’Connor <daniel@danoc.me>
    
    * Fixing change skipped in revert
    
    * Update packages/thumbprint-react/components/Image/index.jsx
    
    Co-Authored-By: Daniel O’Connor <daniel@danoc.me>
    
    * Update changelog, add 8:5 info

--- packages/thumbprint-react/components/Image/index.jsx
+++ packages/thumbprint-react/components/Image/index.jsx
@@ -1,21 +1,33 @@
 import React, { useState, forwardRef, useEffect } from 'react';
+import find from 'lodash/find';
+import classNames from 'classnames';
 import warning from 'warning';
 import PropTypes from 'prop-types';
 import scrollparent from 'scrollparent';
 import { useInView } from 'react-intersection-observer';
-import Picture from './components/picture.jsx';
 import canUseDOM from '../../utils/can-use-dom';
+import styles from './index.module.scss';
+
+// --------------------------------------------------------------------------------------------
+// Steps in rendering Image
+//
+// 1. Picture is rendered without src, srcSets, and with a padding-top placholder on the <img>
+// based on the containerAspectRatio.
+// 2. The "sizes" attr is calculated on initial render to determine width of image.
+// 3. When lazyload is triggered the src and scrSet props are populated based on the sizes value.
+// 4. The image onLoad event removes padding-top placholder and fades in the image.
+// --------------------------------------------------------------------------------------------
 
 const Image = forwardRef((props, outerRef) => {
     const {
         src,
         sources,
-        style,
         height,
         containerAspectRatio,
         objectFit,
         objectPosition,
         alt,
+        className,
         ...rest
     } = props;
 
@@ -24,30 +36,14 @@ const Image = forwardRef((props, outerRef) => {
     const [containerRef, setContainerRef] = useState(null);
 
     // --------------------------------------------------------------------------------------------
-    // Inline styles for container `div` and `Picture` component
+    // Sizes
     // --------------------------------------------------------------------------------------------
 
-    const pictureProps = {
-        src,
-        sources,
-        alt,
-        style: { width: '100%', height: height || '100%', display: 'block' },
-        sizes: containerRef && containerRef.clientWidth ? `${containerRef.clientWidth}px` : '0px',
-    };
+    // Used by srcSet to determine which image in the list will be requested. This value has to be
+    // calculated client-side because we don't know the viewport width.
 
-    const containerProps = {
-        ...rest,
-        style: {
-            ...style,
-            // Allows the container to behave like an image would. Without this, something like
-            // passing a `border-radius` in `className` or `style` would not work since the
-            // container is a `div`, not an `img`.
-            overflow: 'hidden',
-            // Setting a `min-height` makes the lazy-loading work in cases where the `Image` parent
-            // has a `0px` height.
-            minHeight: '1px',
-        },
-    };
+    const sizes =
+        containerRef && containerRef.clientWidth ? `${containerRef.clientWidth}px` : '0px';
 
     // --------------------------------------------------------------------------------------------
     // Lazy-loading: library setup and polyfill
@@ -58,6 +54,7 @@ const Image = forwardRef((props, outerRef) => {
     // of using the default, we use the nearest scrollable parent. This is the same approach that
     // React Waypoint and lazysizes use. The React Waypoint README explains this concept well:
     // https://git.io/fj00H
+
     const parent = canUseDOM && scrollparent(containerRef);
     const root = parent && (parent.tagName === 'HTML' || parent.tagName === 'BODY') ? null : parent;
 
@@ -84,7 +81,9 @@ const Image = forwardRef((props, outerRef) => {
     // Object Fit: polyfill and CSS styles
     // --------------------------------------------------------------------------------------------
 
-    const shouldObjectFit = height || containerAspectRatio;
+    const objectFitProps = {};
+
+    const shouldObjectFit = !!height;
     const shouldPolyfillObjectFit =
         canUseDOM &&
         document.documentElement &&
@@ -114,80 +113,120 @@ const Image = forwardRef((props, outerRef) => {
     );
 
     if (shouldObjectFit) {
-        pictureProps.style = {
-            ...pictureProps.style,
+        objectFitProps.style = {
             objectFit,
             objectPosition,
         };
-
         if (shouldPolyfillObjectFit) {
             // Weird, but this is how the polyfill knows what to do with the image in IE.
-            pictureProps.style.fontFamily = `"object-fit: ${objectFit}; object-position: ${objectPosition}"`;
+            objectFitProps.style.fontFamily = `"object-fit: ${objectFit}; object-position: ${objectPosition}"`;
         }
     }
 
     // --------------------------------------------------------------------------------------------
-    // Aspect Ratio Boxes
+    // Image Aspect Ratio used for image placeholder
     // --------------------------------------------------------------------------------------------
 
     const aspectRatioBoxProps = {};
 
     if (containerAspectRatio) {
-        // This ensures that the image always renders at that apsect ratio and that lazy-loaded
-        // images don't cause the browser scroll to jump once the image has loaded. It uses the
-        // following technique: https://css-tricks.com/aspect-ratio-boxes/
+        // This ensures that lazy-loaded images don't cause the browser scroll to jump once the
+        // image has loaded. It uses the following technique:
+        // https://css-tricks.com/aspect-ratio-boxes/
         const h = 100000;
         const w = h * containerAspectRatio;
 
-        containerProps.style = {
-            ...containerProps.style,
-            position: 'relative',
-        };
-
         aspectRatioBoxProps.style = {
             paddingTop: `${(h / w) * 100}%`,
-        };
-
-        pictureProps.style = {
-            ...pictureProps.style,
-            position: 'absolute',
-            top: 0,
-            right: 0,
-            bottom: 0,
-            left: 0,
-            height: '100%',
+            height: 0,
         };
     }
 
+    // --------------------------------------------------------------------------------------------
+    // Sources and srcSets
+    // --------------------------------------------------------------------------------------------
+
+    // We separate `webp` from the `jpeg`/`png` so that we can apply the `imgTagSource` directly
+    // onto the `img` tag. While this makes the code messier, it is needed to work around a bug in
+    // Safari:
+    // - https://bugs.webkit.org/show_bug.cgi?id=190031
+    // - https://bugs.webkit.org/show_bug.cgi?id=177068
+
+    const webpSource = find(sources, s => s.type === 'image/webp');
+    const imgTagSource = find(sources, s => s.type === 'image/jpeg' || s.type === 'image/png');
+
+    // --------------------------------------------------------------------------------------------
+    // Image loaded fade in
+    // --------------------------------------------------------------------------------------------
+
+    const [isLoaded, setIsLoaded] = useState(false);
+
     return (
-        <div
-            {...containerProps}
-            ref={el => {
-                // Using a callback `ref` on this `div` allows us to have multiple `ref`s on one
-                // element.
-                setContainerRef(el);
+        <>
+            <picture
+                {...rest}
+                className={classNames(styles.picture, className)}
+                ref={el => {
+                    // Using a callback `ref` on this `picture` allows us to have multiple `ref`s on one
+                    // element.
+                    setContainerRef(el);
 
-                // We don't want to turn on the `react-intersection-observer` functionality until
-                // the polyfill is done loading.
-                if (browserSupportIntersectionObserver) {
-                    inViewRef(el);
-                }
+                    // We don't want to turn on the `react-intersection-observer` functionality until
+                    // the polyfill is done loading.
+                    if (browserSupportIntersectionObserver) {
+                        inViewRef(el);
+                    }
 
-                // `outerRef` is the potential forwarded `ref` passed in from a consumer.
-                if (outerRef) {
-                    outerRef(el);
-                }
-            }}
-        >
-            {/* This `div` holds the space set by `containerAspectRatio`. */}
-            {Object.keys(aspectRatioBoxProps).length > 0 && <div {...aspectRatioBoxProps} />}
-
-            {shouldLoadImage && <Picture {...pictureProps} />}
-
+                    // `outerRef` is the potential forwarded `ref` passed in from a consumer.
+                    if (outerRef) {
+                        outerRef(el);
+                    }
+                }}
+            >
+                {webpSource && (
+                    <source
+                        type={webpSource.type}
+                        // Only add this attribute if lazyload has been triggered.
+                        srcSet={shouldLoadImage ? webpSource.srcSet : undefined}
+                        sizes={sizes}
+                    />
+                )}
+                <img
+                    // The order of `sizes`, `srcSet`, and `src` is important to work around a bug in
+                    // Safari. Once the bug is fixed, we should simplify this by using `src` on the
+                    // `img` tag and using `source` tags.
+                    sizes={sizes}
+                    // Only add this attribute if lazyload has been triggered.
+                    srcSet={shouldLoadImage && imgTagSource ? imgTagSource.srcSet : undefined}
+                    // Only add this attribute if lazyload has been triggered.
+                    src={shouldLoadImage ? src : undefined}
+                    // Height is generally only used for full-width hero images.
+                    height={height}
+                    alt={alt}
+                    // Adds object fit values if specified and adds/removes placeholder padding.
+                    style={{
+                        ...(shouldObjectFit ? objectFitProps.style : {}),
+                        ...(isLoaded ? {} : aspectRatioBoxProps.style),
+                    }}
+                    // Once loaded we remove the placeholder and add a class to transition the
+                    // opacity from 0 to 1.
+                    onLoad={() => {
+                        setIsLoaded(true);
+                    }}
+                    className={classNames({
+                        [styles.image]: true,
+                        [styles.imageLoading]: true,
+                        [styles.imageLoaded]: isLoaded,
+                    })}
+                />
+            </picture>
             <noscript>
-                <Picture src={src} sources={sources} alt={alt} />
+                <picture>
+                    {webpSource && <source srcSet={webpSource.srcSet} type={webpSource.type} />}
+                    <img src={src} alt={alt} srcSet={imgTagSource && imgTagSource.srcSet} />
+                </picture>
             </noscript>
-        </div>
+        </>
     );
 });
 
@@ -216,21 +255,18 @@ Image.propTypes = {
      */
     height: PropTypes.string,
     /**
-     * Crops the image to a specific aspect ratio and creates a [placeholder box](https://css-tricks.com/aspect-ratio-boxes/)
-     * for the image. The placeholder prevents the browser scroll from jumping when the image is
-     * lazy-loaded.
+     * Creates a [placeholder box](https://css-tricks.com/aspect-ratio-boxes/) for the image.
+     * The placeholder prevents the browser scroll from jumping when the image is lazy-loaded.
      */
     containerAspectRatio: PropTypes.number,
     /**
-     * Provides control over how an image should be resized to fit the container. This controls the
-     * `object-fit` CSS property. It is only useful if `height` or `containerAspectRatio` are used to
-     * "crop" an image.
+     * Provides control over how the image should be resized to fit the container. This controls the
+     * `object-fit` CSS property. It is only useful if `height` is used to "crop" the image.
      */
     objectFit: PropTypes.oneOf(['cover', 'contain']),
     /**
-     * Provides control over how an image aligned in the container. This controls the
-     * `object-position` CSS property. It is only useful if `height` or `containerAspectRatio` are used to
-     * "crop" an image.
+     * Provides control over how the image position in the container. This controls the
+     * `object-position` CSS property. It is only useful if `height` is used to "crop" the image.
      */
     objectPosition: PropTypes.oneOf(['top', 'center', 'bottom', 'left', 'right']),
 };
