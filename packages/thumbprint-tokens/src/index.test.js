7005b8d00854284f9098f90a8008337aa85c5843
Daniel O'Connor <doconnor@thumbtack.com>
2019-06-25 15:59:54 -0700

    Fix snapshot tests.

--- packages/thumbprint-tokens/src/index.test.js
+++ packages/thumbprint-tokens/src/index.test.js
@@ -1,3 +1,4 @@
+const JSZip = require('jszip');
 const fs = require('fs');
 
 it('outputted CommonJS matches snapshot', () => {
@@ -21,7 +22,12 @@ it('outputted SCSS matches snapshot', () => {
     expect(output).toMatchSnapshot();
 });
 
-it('outputted iOS matches snapshot', () => {
-    const output = fs.readFileSync('packages/thumbprint-tokens/dist/ios/index.swift', 'utf-8');
+it('outputted iOS matches snapshot', async () => {
+    // The output is stored as a `zip`, but we unzip it for the purpose of this test so that we
+    // can compare the token file as a string.
+    const zipBuffer = fs.readFileSync('packages/thumbprint-tokens/dist/ios.zip');
+    const files = await JSZip.loadAsync(zipBuffer);
+    const output = await files.file('index.swift').async('string');
+
     expect(output).toMatchSnapshot();
 });
