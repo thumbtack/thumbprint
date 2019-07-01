e4bd73d0bcbe11556dffa51ac896292e59690347
Daniel O'Connor <doconnor@thumbtack.com>
2019-07-01 16:44:52 -0700

    Fix build.

--- packages/thumbprint-react/utils/ConditionalPortal.jsx
+++ packages/thumbprint-react/utils/ConditionalPortal.jsx
@@ -4,7 +4,7 @@ import PropTypes from 'prop-types';
 import canUseDOM from './can-use-dom';
 
 /**
- * Component to conditionally portal a component to the end of the <body> if a certain condition is
+ * Component to conditionally portal a component to the end of the `<body>` if a certain condition is
  * true. Also automatically guards against trying to use portals in SSR where `document` is not
  * defined.
  */
