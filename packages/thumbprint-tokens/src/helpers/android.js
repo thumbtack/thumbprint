77b6d5148fd66c9a1e959e130ebf723cce37e00d
Daniel O'Connor <doconnor@thumbtack.com>
2019-07-11 16:39:17 -0700

    Create Tokens for Android
    This creates the tokens but does not publish or document them. Those will be follow-up tasks.

---
+++ packages/thumbprint-tokens/src/helpers/android.js
@@ -0,0 +1,44 @@
+const getTag = type => {
+    // This is a separate helper because it's hard to do complicated conditionals within
+    // `.handlebars` files. This function is similar to:
+    // https://github.com/amzn/style-dictionary/blob/master/examples/advanced/custom-formats-with-templates/templates/android-xml.template
+    if (type === 'color') {
+        return 'color';
+    }
+
+    if (type === 'size') {
+        return 'dimen';
+    }
+
+    return 'item';
+};
+
+module.exports = [
+    { name: 'formatValue', value: ({ value }) => value.android },
+    {
+        name: 'formatId',
+        value: (section, token) => {
+            const formattedSection = section.name.replace(/\s+/g, '_').toLowerCase();
+            const formattedToken = token.id.replace(/-/g, '_');
+
+            // This conditional ensures that `<color name="red_300">#ffb0b0</color>` doesn't get
+            // printed as `<color name="color_red_300">#ffb0b0</color>` since including the type
+            // in the name is redundant in such a case.
+            if (token.type === getTag(token.type)) {
+                return formattedToken;
+            }
+
+            return `${formattedSection}_${formattedToken}`;
+        },
+    },
+    {
+        name: 'formatSectionName',
+        value: ({ name }) =>
+            // Changes 'Border Radius' to 'BorderRadius'.
+            name.replace(/\s/g, ''),
+    },
+    {
+        name: 'getTag',
+        value: getTag,
+    },
+];
--- packages/thumbprint-tokens/src/helpers/ios.js
+++ packages/thumbprint-tokens/src/helpers/ios.js
@@ -47,10 +47,4 @@ module.exports = [
             return result.join('');
         },
     },
-    {
-        name: 'ifEquals',
-        value(arg1, arg2, options) {
-            return arg1 === arg2 ? options.fn(this) : options.inverse(this);
-        },
-    },
 ];
