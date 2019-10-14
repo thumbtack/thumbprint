34fe32a75b093b15a804cee82924a975376419e9
Brian Lee <blee@thumbtack.com>
2019-10-14 16:34:11 -0700

    Add isRequired PropType for Checkbox, Radio

--- packages/thumbprint-react/components/Checkbox/index.tsx
+++ packages/thumbprint-react/components/Checkbox/index.tsx
@@ -128,6 +128,10 @@ interface PropTypes {
      */
     id?: string;
     /**
+     * Adds the `required` HTML attribute.
+     */
+    isRequired?: boolean;
+    /**
      * Checkboxes on a page with the same name will be grouped together when sent to the server.
      * The browser will only send the value of checkboxes that are checked.
      */
@@ -168,6 +172,7 @@ export default function Checkbox({
     isDisabled = false,
     isIndeterminate = false,
     labelPadding = '14px 0',
+    isRequired = false,
     name,
     onChange,
 }: PropTypes): JSX.Element {
@@ -198,6 +203,7 @@ export default function Checkbox({
                 checked={isChecked}
                 onChange={(event): void => onChange(event.target.checked, id)}
                 disabled={isDisabled}
+                required={isRequired}
             />
 
             <div
