ff9ef6802a64aadc95a206305be81daf91024708
Daniel O'Connor <doconnor@thumbtack.com>
2019-10-17 11:59:28 -0700

    Improvement to preview design of Netlify CMS post
    It previously had no CSS.

---
+++ www/src/cms/cms.js
@@ -0,0 +1,8 @@
+/**
+ * The default export of `netlify-cms-app` is an object with all of the Netlify CMS
+ * extension registration methods, such as `registerWidget` and
+ * `registerPreviewTemplate`.
+ */
+import CMS from 'netlify-cms-app';
+
+CMS.registerPreviewStyle('https://unpkg.com/picnic@6.5.1/picnic.min.css');
