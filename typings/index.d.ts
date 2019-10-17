11d0ebfdecb9126cbad3ecb2cac80245a0c62b08
lavelle <giles.lavelle@gmail.com>
2019-10-17 19:12:00 +0100

    Remove tokens stub type (#508)

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -1,8 +1,6 @@
 // Silence errors from libraries with no type defintions provided
 declare module 'intersection-observer';
 declare module 'object-fit-images';
-// TODO: add types to tokens and remove this line
-declare module '@thumbtack/thumbprint-tokens';
 
 declare module 'rotate-array' {
     function rotate<T>(array: Array<T>, offset: number): Array<T>;
