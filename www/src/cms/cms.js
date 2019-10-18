9e546c612d639ab5d49c50b25049db713c202c04
Daniel O'Connor <doconnor@thumbtack.com>
2019-10-18 14:27:09 -0700

    Set up a Figma widget

--- www/src/cms/cms.js
+++ www/src/cms/cms.js
@@ -6,3 +6,17 @@
 import CMS from 'netlify-cms-app';
 
 CMS.registerPreviewStyle('https://unpkg.com/picnic@6.5.1/picnic.min.css');
+
+CMS.registerEditorComponent({
+    id: 'figma',
+    label: 'Figma',
+    fields: [{ name: 'id', label: 'Figma URL', widget: 'string' }],
+    // eslint-disable-next-line no-useless-escape
+    pattern: /https:\/\/([\w\.-]+\.)?figma.com\/(file|proto)\/([0-9a-zA-Z]{22,128})(?:\/.*)?$/,
+    fromBlock: match => ({
+        id: match[0],
+    }),
+    toBlock: obj => obj.id,
+    toPreview: obj =>
+        `<iframe height="500" width="100%" src="https://www.figma.com/embed?embed_host=astra&url=${obj.id}" allowfullscreen></iframe>`,
+});
