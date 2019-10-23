2d92d0ba5615e2bfd81fd5311b2cac524b21123d
Giles Lavelle <giles.lavelle@gmail.com>
2019-10-23 20:12:15 +0100

    Convert Button to TypeScript

--- packages/thumbprint-react/components/Button/test.tsx
+++ packages/thumbprint-react/components/Button/test.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 import { mount } from 'enzyme';
 import Button, { TextButton } from './index';
 
-test('renders children passed in', () => {
-    const testComponent = Component => {
+test('renders children passed in', (): void => {
+    const testComponent = (Component: React.ElementType): void => {
         const wrapper = mount(<Component>Goose</Component>);
         expect(wrapper.text()).toEqual('Goose');
         expect(wrapper).toMatchSnapshot();
@@ -13,8 +13,8 @@ test('renders children passed in', () => {
     testComponent(TextButton);
 });
 
-test('renders button of type `button` by default', () => {
-    const testComponent = Component => {
+test('renders button of type `button` by default', (): void => {
+    const testComponent = (Component: React.ElementType): void => {
         const wrapper = mount(<Component>Goose</Component>);
         expect(wrapper.find('button').prop('type')).toEqual('button');
         expect(wrapper).toMatchSnapshot();
@@ -24,8 +24,8 @@ test('renders button of type `button` by default', () => {
     testComponent(TextButton);
 });
 
-test('renders button of type `submit` when `type` is set', () => {
-    const testComponent = Component => {
+test('renders button of type `submit` when `type` is set', (): void => {
+    const testComponent = (Component: React.ElementType): void => {
         const wrapper = mount(<Component type="submit">Goose</Component>);
         expect(wrapper.find('button').prop('type')).toEqual('submit');
         expect(wrapper).toMatchSnapshot();
@@ -35,8 +35,8 @@ test('renders button of type `submit` when `type` is set', () => {
     testComponent(TextButton);
 });
 
-test('`onClick` runs when button is clicked on', () => {
-    const testComponent = Component => {
+test('`onClick` runs when button is clicked on', (): void => {
+    const testComponent = (Component: React.ElementType): void => {
         const onClick = jest.fn();
         const wrapper = mount(<Component onClick={onClick}>Goose</Component>);
 
@@ -49,8 +49,8 @@ test('`onClick` runs when button is clicked on', () => {
     testComponent(TextButton);
 });
 
-test('`onMouseOver` runs when button is moused over', () => {
-    const testComponent = Component => {
+test('`onMouseOver` runs when button is moused over', (): void => {
+    const testComponent = (Component: React.ElementType): void => {
         const onMouseOver = jest.fn();
         // eslint-disable-next-line jsx-a11y/mouse-events-have-key-events
         const wrapper = mount(<Component onMouseOver={onMouseOver}>Goose</Component>);
@@ -64,14 +64,14 @@ test('`onMouseOver` runs when button is moused over', () => {
     testComponent(TextButton);
 });
 
-test('renders icon that is passed in', () => {
+test('renders icon that is passed in', (): void => {
     const wrapper = mount(<Button icon={<svg>Duck</svg>}>Goose</Button>);
     expect(wrapper.find('svg')).toHaveLength(1);
     expect(wrapper.text()).toBe('DuckGoose');
     expect(wrapper).toMatchSnapshot();
 });
 
-test('adds `dataTest` prop', () => {
+test('adds `dataTest` prop', (): void => {
     const wrapperA = mount(<Button dataTest="Duck">Goose</Button>);
     expect(wrapperA.find('button').prop('data-test')).toEqual('Duck');
     expect(wrapperA).toMatchSnapshot();
