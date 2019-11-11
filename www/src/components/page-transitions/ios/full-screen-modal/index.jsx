cd79bee9134348f2bc5d78f5a17150a822613b59
Tom Genoni <tom@thumbtack.com>
2019-11-11 15:50:44 -0800

    Update copy

--- www/src/components/page-transitions/ios/full-screen-modal/index.jsx
+++ www/src/components/page-transitions/ios/full-screen-modal/index.jsx
@@ -6,16 +6,16 @@ import sharedStyle from '../../shared.module.scss';
 const IosFullScreenModal = () => {
     const [active, setActive] = useState(false);
     return (
-        <button className={sharedStyle.root} onClick={() => setActive(!active)} type="button">
-            <div
-                className={classNames({
-                    'h-100': true,
-                    [localStyle.isActive]: active,
-                })}
-            >
-                <div className={`${sharedStyle.box} bg-white`}>A</div>
-                <div className={`${sharedStyle.box} ${localStyle.box2} bg-black white`}>B</div>
-            </div>
+        <button
+            className={classNames({
+                [sharedStyle.root]: true,
+                [localStyle.isActive]: active,
+            })}
+            onClick={() => setActive(!active)}
+            type="button"
+        >
+            <div className={`${sharedStyle.box} bg-white`}>A</div>
+            <div className={`${sharedStyle.box} ${localStyle.box2} bg-black white`}>B</div>
         </button>
     );
 };
--- www/src/components/page-transitions/ios/page-sheet/index.jsx
+++ www/src/components/page-transitions/ios/page-sheet/index.jsx
@@ -6,16 +6,16 @@ import sharedStyle from '../../shared.module.scss';
 const IosPageSheet = () => {
     const [active, setActive] = useState(false);
     return (
-        <button className={sharedStyle.root} onClick={() => setActive(!active)} type="button">
-            <div
-                className={classNames({
-                    'h-100': true,
-                    [localStyle.isActive]: active,
-                })}
-            >
-                <div className={`${sharedStyle.box} ${localStyle.box1} bg-white`}>A</div>
-                <div className={`${sharedStyle.box} ${localStyle.box2} bg-white br3`}>B</div>
-            </div>
+        <button
+            className={classNames({
+                [sharedStyle.root]: true,
+                [localStyle.isActive]: active,
+            })}
+            onClick={() => setActive(!active)}
+            type="button"
+        >
+            <div className={`${sharedStyle.box} ${localStyle.box1} bg-white`}>A</div>
+            <div className={`${sharedStyle.box} ${localStyle.box2} bg-white br3`}>B</div>
         </button>
     );
 };
