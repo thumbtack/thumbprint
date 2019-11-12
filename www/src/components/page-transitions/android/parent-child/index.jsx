cd1ab5c994346cf867af19e0528c42fb6a9915aa
Tom Genoni <tom@thumbtack.com>
2019-11-12 10:32:02 -0800

    Update styles

--- www/src/components/page-transitions/android/parent-child/index.jsx
+++ www/src/components/page-transitions/android/parent-child/index.jsx
@@ -16,36 +16,27 @@ const AndroidParentChild = () => {
     return (
         <div className={style.root}>
             <button
-                className={classNames(
-                    `${style.box} ${style.box0} flex items-center justify-center bg-white`,
-                    {
-                        [style.isActive]: tab === 0,
-                    },
-                )}
+                className={classNames(`${style.box} ${style.box0} bg-white`, {
+                    [style.isActive]: tab === 0,
+                })}
                 onClick={() => handleClick(0)}
                 type="button"
             >
                 A
             </button>
             <button
-                className={classNames(
-                    `${style.box} ${style.box1} flex items-center justify-center bg-gray-300`,
-                    {
-                        [style.isActive]: tab === 1,
-                    },
-                )}
+                className={classNames(`${style.box} ${style.box1} bg-gray-300`, {
+                    [style.isActive]: tab === 1,
+                })}
                 onClick={() => handleClick(1)}
                 type="button"
             >
                 B
             </button>
             <button
-                className={classNames(
-                    `${style.box} ${style.box2} flex items-center justify-center bg-gray`,
-                    {
-                        [style.isActive]: tab === 2,
-                    },
-                )}
+                className={classNames(`${style.box} ${style.box2} bg-gray`, {
+                    [style.isActive]: tab === 2,
+                })}
                 onClick={() => handleClick(2)}
                 type="button"
             >
