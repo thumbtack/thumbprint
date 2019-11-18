ee343d5ae778d69165e7aa9b57fb94a39a51fb28
Tom Genoni <tom@thumbtack.com>
2019-11-18 14:08:54 -0800

    Add examples

--- www/src/components/skeleton-loader/index.jsx
+++ www/src/components/skeleton-loader/index.jsx
@@ -16,46 +16,48 @@ const aspectMap = {
     '10:13': 13 / 10,
 };
 
-export default function Skeleton({ isBlock, aspectRatio, width, height }) {
-    const inline = {};
+const Skeleton = ({ isBlock, aspectRatio, width, height, className }) => {
+    const inlineStyles = {};
 
     if (aspectRatio) {
-        inline.paddingTop = getAspectPercentage(aspectMap[aspectRatio]);
-        inline.height = 0;
+        inlineStyles.paddingTop = getAspectPercentage(aspectMap[aspectRatio]);
+        inlineStyles.height = 0;
     }
 
     if (width && !aspectRatio) {
-        inline.width = width;
+        inlineStyles.width = width;
     }
 
     if (height && !aspectRatio) {
-        inline.height = width;
+        inlineStyles.height = width;
     }
 
     return (
         <span
             className={classNames({
                 [styles.skeleton]: true,
+                [className]: className,
                 db: isBlock || aspectRatio,
             })}
-            style={inline}
+            style={inlineStyles}
         />
     );
-}
+};
 
-const propTypes = {
+Skeleton.propTypes = {
     width: PropTypes.string,
     height: PropTypes.string,
     isBlock: PropTypes.bool,
     aspectRatio: PropTypes.string,
+    className: PropTypes.string,
 };
 
-const defaultProps = {
+Skeleton.defaultProps = {
     width: null,
     height: null,
     isBlock: false,
     aspectRatio: null,
+    className: null,
 };
 
-Skeleton.propTypes = propTypes;
-Skeleton.defaultProps = defaultProps;
+export default Skeleton;
