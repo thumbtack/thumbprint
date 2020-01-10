047f00278f011a625439dfe8db92a4679bec9f8d
Tom Genoni <tom@thumbtack.com>
2020-01-10 13:30:48 -0800

    Filter out duplicate classname, hack

--- www/src/components/thumbprint-atomic/table/index.jsx
+++ www/src/components/thumbprint-atomic/table/index.jsx
@@ -10,45 +10,52 @@ const cleanSelector = selector =>
         .replace(/hover.*:focus/, '')
         .replace(/.*> \*/, '');
 
-const Table = ({ atomicClasses }) => (
-    <table className="w-100 tp-body-2">
-        <tbody>
-            {atomicClasses.map(classItem => (
-                <tr key={classItem.selectors.join('')} className="bb b-gray-300">
-                    <td className="v-top w-40 pv2">
-                        <table>
-                            <tbody>
-                                {classItem.selectors.map(v => (
-                                    <tr key={v}>
-                                        <td data-algolia="include">
-                                            <InlineCode theme="plain" shouldCopyToClipboard>
-                                                {cleanSelector(v)}
-                                            </InlineCode>
-                                        </td>
-                                    </tr>
-                                ))}
-                            </tbody>
-                        </table>
-                    </td>
-                    <td className="v-top w-60 pv2 black-300">
-                        <table>
-                            <tbody>
-                                {classItem.declarations.map(v => (
-                                    <tr key={v}>
-                                        <td>
-                                            <InlineCode theme="plain">{v}</InlineCode>
-                                            <Swatch value={v} />
-                                        </td>
-                                    </tr>
-                                ))}
-                            </tbody>
-                        </table>
-                    </td>
-                </tr>
-            ))}
-        </tbody>
-    </table>
-);
+const Table = ({ atomicClasses }) => {
+    const atomicClassesHackless = atomicClasses.filter(
+        // If `_:-ms-lang(x)` is a classname, remove it from the array. Also prevents duplicated
+        // classname that is in the source code for IE/Edge browsers from rendering in these docs.
+        item => item.selectors[1] !== '_:-ms-lang(x)',
+    );
+    return (
+        <table className="w-100 tp-body-2">
+            <tbody>
+                {atomicClassesHackless.map(classItem => (
+                    <tr key={classItem.selectors.join('')} className="bb b-gray-300">
+                        <td className="v-top w-40 pv2">
+                            <table>
+                                <tbody>
+                                    {classItem.selectors.map(v => (
+                                        <tr key={v}>
+                                            <td data-algolia="include">
+                                                <InlineCode theme="plain" shouldCopyToClipboard>
+                                                    {cleanSelector(v)}
+                                                </InlineCode>
+                                            </td>
+                                        </tr>
+                                    ))}
+                                </tbody>
+                            </table>
+                        </td>
+                        <td className="v-top w-60 pv2 black-300">
+                            <table>
+                                <tbody>
+                                    {classItem.declarations.map(v => (
+                                        <tr key={v}>
+                                            <td>
+                                                <InlineCode theme="plain">{v}</InlineCode>
+                                                <Swatch value={v} />
+                                            </td>
+                                        </tr>
+                                    ))}
+                                </tbody>
+                            </table>
+                        </td>
+                    </tr>
+                ))}
+            </tbody>
+        </table>
+    );
+};
 
 Table.propTypes = {
     atomicClasses: PropTypes.arrayOf(PropTypes.shape({})).isRequired,
