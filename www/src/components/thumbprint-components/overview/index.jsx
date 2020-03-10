d65cc7d746ff2f25f8b9a2c78466236a0a780fa8
Tom Genoni <tom@thumbtack.com>
2020-03-10 14:52:44 -0700

    Add component boxes

--- www/src/components/thumbprint-components/overview/index.jsx
+++ www/src/components/thumbprint-components/overview/index.jsx
@@ -44,103 +44,17 @@ Dot.propTypes = {
     status: documentationStatus.isRequired,
 };
 
-export const Legend = () => (
-    <ul aria-hidden="true" className="flex black-300 mb2">
-        <li className="mr4 flex items-center">
-            <Dot status="Done / Merged" />{' '}
-            <Text size={2} className="ml2">
-                Done
-            </Text>
-        </li>
-        <li className="mr4 flex items-center">
-            <Dot status="In progress" />{' '}
-            <Text size={2} className="ml2">
-                In Progress
-            </Text>
-        </li>
-        <li className="mr4 flex items-center">
-            <Dot status="To-do" />{' '}
-            <Text size={2} className="ml2">
-                To-do
-            </Text>
-        </li>
-        <li className="mr4 flex items-center">
-            <Dot status="Done / Won't build" />{' '}
-            <Text size={2} className="ml2">
-                Not applicable
-            </Text>
-        </li>
-        <li className="mr4 flex items-center">
-            <Dot status="Done / Deprecated" />{' '}
-            <Text size={2} className="ml2">
-                Deprecated
-            </Text>
-        </li>
-    </ul>
-);
+export const Legend = () => <div>search tools</div>;
 
 export const ComponentRow = ({ name, react, scss, ios, android }) => (
-    <>
-        <tr className="tl bb b-gray-300">
-            <th rowSpan={4} className="pr2 pv2 v-top bb b-gray-300 bw-2">
-                {name}
-            </th>
-            <th className="normal ph2 pv1">
-                <Text size={2}>React</Text>
-            </th>
-            <td className="pa2 tc">
-                <Dot status={react.design} />
-            </td>
-            <td className="pa2 tc">
-                <Dot status={react.development} />
-            </td>
-            <td className="pa2 tc">
-                <Dot status={react.documentation} />
-            </td>
-        </tr>
-        <tr className="bb b-gray-300 tl">
-            <th className="normal ph2 pv1">
-                <Text size={2}>SCSS</Text>
-            </th>
-            <td className="pa2 tc">
-                <Dot status={scss.design} />
-            </td>
-            <td className="pa2 tc">
-                <Dot status={scss.development} />
-            </td>
-            <td className="pa2 tc">
-                <Dot status={scss.documentation} />
-            </td>
-        </tr>
-        <tr className="bb b-gray-300 tl">
-            <th className="normal ph2 pv1">
-                <Text size={2}>iOS</Text>
-            </th>
-            <td className="pa2 tc">
-                <Dot status={ios.design} />
-            </td>
-            <td className="pa2 tc">
-                <Dot status={ios.development} />
-            </td>
-            <td className="pa2 tc">
-                <Dot status={ios.documentation} />
-            </td>
-        </tr>
-        <tr className="bb b-gray-300 bw-2 tl">
-            <th className="normal ph2 pv1">
-                <Text size={2}>Android</Text>
-            </th>
-            <td className="pa2 tc">
-                <Dot status={android.design} />
-            </td>
-            <td className="pa2 tc">
-                <Dot status={android.development} />
-            </td>
-            <td className="pa2 tc">
-                <Dot status={android.documentation} />
-            </td>
-        </tr>
-    </>
+    <a href="/path/" className={`color-inherit pa2 ba b-gray-300 br2 ${styles.component}`}>
+        <div className="h4 bg-gray br1 mb1">&nbsp;</div>
+        <Text size={2}>{name}</Text>
+        <Text size={3} className="black-300">
+            <span>Status: </span>
+            <span>{react.documentation}</span>
+        </Text>
+    </a>
 );
 
 ComponentRow.propTypes = {
@@ -173,65 +87,7 @@ ComponentRow.propTypes = {
 };
 
 export const ComponentTable = ({ children }) => (
-    <table className="w-100">
-        <thead className="tl">
-            <tr>
-                {/*
-                    The `boxShadow` is used instead of `border` because of:
-                    https://stackoverflow.com/q/41882616/316602
-                 */}
-                <th
-                    className="pt3 pr2 pb2 v-top top0 bg-white"
-                    style={{
-                        position: 'sticky',
-                        boxShadow: `0px 2px 0px 0px ${tpColorGray300}`,
-                    }}
-                >
-                    <Text size={2}>Component</Text>
-                </th>
-                <th
-                    className="pt3 ph2 pb2 v-top top0 bg-white"
-                    style={{
-                        position: 'sticky',
-                        boxShadow: `0px 2px 0px 0px ${tpColorGray300}`,
-                    }}
-                >
-                    <Text size={2}>Platforms</Text>
-                </th>
-                <th
-                    className="pt3 ph3 pb2 v-top top0 bg-white"
-                    style={{
-                        width: '1%',
-                        position: 'sticky',
-                        boxShadow: `0px 2px 0px 0px ${tpColorGray300}`,
-                    }}
-                >
-                    <Text size={2}>Design</Text>
-                </th>
-                <th
-                    className="pt3 ph3 pb2 v-top top0 bg-white"
-                    style={{
-                        width: '1%',
-                        position: 'sticky',
-                        boxShadow: `0px 2px 0px 0px ${tpColorGray300}`,
-                    }}
-                >
-                    <Text size={2}>Development</Text>
-                </th>
-                <th
-                    className="pt3 ph3 pb2 v-top top0 bg-white"
-                    style={{
-                        width: '1%',
-                        position: 'sticky',
-                        boxShadow: `0px 2px 0px 0px ${tpColorGray300}`,
-                    }}
-                >
-                    <Text size={2}>Documentation</Text>
-                </th>
-            </tr>
-        </thead>
-        <tbody>{children}</tbody>
-    </table>
+    <div className={styles.componentGrid}>{children}</div>
 );
 
 ComponentTable.propTypes = {
