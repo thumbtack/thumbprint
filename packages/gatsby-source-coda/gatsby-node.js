c925aefd8863814baaa5204ca58d0c718cae915a
Daniel O'Connor <doconnor@thumbtack.com>
2020-03-17 14:18:08 -0700

    Do some refactoring

--- packages/gatsby-source-coda/gatsby-node.js
+++ packages/gatsby-source-coda/gatsby-node.js
@@ -2,7 +2,7 @@ const crypto = require('crypto');
 const fetch = require('node-fetch');
 const queryString = require('query-string');
 
-exports.sourceNodes = async ({ actions }, { apiToken, docId, tableIdOrName, useColumnNames }) => {
+exports.sourceNodes = async ({ actions, reporter }, { apiToken, tables }) => {
     if (!apiToken) {
         return;
     }
@@ -10,60 +10,80 @@ exports.sourceNodes = async ({ actions }, { apiToken, docId, tableIdOrName, useC
     const { createNode } = actions;
 
     const qs = queryString.stringify({
-        useColumnNames,
+        useColumnNames: true,
     });
 
-    let listRowsRes;
+    const requests = tables.map(async ({ docId, tableId }) => {
+        let tableNameRes;
+        let listRowsRes;
 
-    try {
-        listRowsRes = await fetch(
-            // https://coda.io/developers/apis/v1beta1#operation/listRows
-            `https://coda.io/apis/v1beta1/docs/${docId}/tables/${tableIdOrName}/rows?${qs}`,
-            {
-                headers: {
-                    Authorization: `Bearer ${apiToken}`,
+        try {
+            tableNameRes = await fetch(
+                // https://coda.io/developers/apis/v1beta1#operation/getTable
+                `https://coda.io/apis/v1beta1/docs/${docId}/tables/${tableId}`,
+                {
+                    headers: {
+                        Authorization: `Bearer ${apiToken}`,
+                    },
                 },
-            },
-        );
-    } catch (error) {
-        throw Error(error);
-    }
+            );
 
-    const data = await listRowsRes.json();
+            listRowsRes = await fetch(
+                // https://coda.io/developers/apis/v1beta1#operation/listRows
+                `https://coda.io/apis/v1beta1/docs/${docId}/tables/${tableId}/rows?${qs}`,
+                {
+                    headers: {
+                        Authorization: `Bearer ${apiToken}`,
+                    },
+                },
+            );
+        } catch (error) {
+            throw Error(error);
+        }
 
-    // Process data into nodes.
-    data.items.forEach(item => {
-        const sanitizedItem = item;
+        const tableName = (await tableNameRes.json()).name;
+        const data = await listRowsRes.json();
 
-        // Sanitize the column names that come from Coda. Spaces get replaced
-        // with an underscore, allowing it to work better with Gatsby.
-        Object.keys(sanitizedItem.values).forEach(key => {
-            const newKey = key.replace(/\s+/g, '_');
-            if (key !== newKey) {
-                sanitizedItem.values[newKey] = sanitizedItem.values[key];
-                delete sanitizedItem.values[key];
-            }
+        if (data.statusCode === 404) {
+            reporter.panic(data.message);
+        }
+
+        // Process data into nodes.
+        data.items.forEach(item => {
+            const sanitizedItem = item;
+
+            // Sanitize the column names that come from Coda. Spaces get replaced
+            // with an underscore, allowing it to work better with Gatsby.
+            Object.keys(sanitizedItem.values).forEach(key => {
+                const newKey = key.replace(/\s+/g, '_');
+                if (key !== newKey) {
+                    sanitizedItem.values[newKey] = sanitizedItem.values[key];
+                    delete sanitizedItem.values[key];
+                }
+            });
+
+            const jsonString = JSON.stringify(sanitizedItem);
+
+            const gatsbyNode = {
+                // Fields from Coda
+                data: sanitizedItem,
+
+                // Required Gatsby fields
+                id: `Coda ${sanitizedItem.id}`,
+                parent: '__SOURCE__',
+                children: [],
+                internal: {
+                    type: `Coda${tableName.replace(/\s+/g, '_')}Table`,
+                    contentDigest: crypto
+                        .createHash('md5')
+                        .update(jsonString)
+                        .digest('hex'),
+                },
+            };
+
+            createNode(gatsbyNode);
         });
-
-        const jsonString = JSON.stringify(sanitizedItem);
-
-        const gatsbyNode = {
-            // Fields from Coda
-            data: sanitizedItem,
-
-            // Required Gatsby fields
-            id: `Coda ${sanitizedItem.id}`,
-            parent: '__SOURCE__',
-            children: [],
-            internal: {
-                type: `Coda${tableIdOrName}Table`,
-                contentDigest: crypto
-                    .createHash('md5')
-                    .update(jsonString)
-                    .digest('hex'),
-            },
-        };
-
-        createNode(gatsbyNode);
     });
+
+    await Promise.all(requests);
 };
--- www/src/components/thumbprint-components/overview/index.jsx
+++ www/src/components/thumbprint-components/overview/index.jsx
@@ -1,138 +1,210 @@
-/* eslint-disable import/prefer-default-export */
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
+import { Link } from 'gatsby';
 import classNames from 'classnames';
 import { Text, TextInput } from '@thumbtack/thumbprint-react';
 import styles from './index.module.scss';
 
-const documentationStatus = PropTypes.oneOf([
-    'To-do',
-    'In progress',
-    "Done / Won't build",
-    'Done / Deprecated',
-    'Done / Merged',
-]);
-
-const developmentStatus = PropTypes.oneOf([
-    'Needs evaluation',
-    'To-do',
-    'In progress',
-    'Done / Uses built in OS component',
-    "Done / Won't build",
-    'Done / Deprecated',
-    'Done / Merged',
-]);
-
-export const FilterBar = () => {
+export default function ComponentOverview({ data }) {
     const [filter, setFilter] = useState(undefined);
-    const [platform, setPlatform] = useState(undefined);
+    const [currentPlatform, setCurrentPlatform] = useState(undefined);
 
     return (
-        <div className="flex mb5">
-            <TextInput size="small" onChange={setFilter} value={filter} />
-            <div className="ba b-gray br2 ml3 flex">
-                <button
-                    type="button"
-                    onClick={() => setPlatform(platform === 'react' ? undefined : 'react')}
-                    className={classNames({
-                        [styles.platformButton]: true,
-                        'br b-gray': true,
-                        [styles.isActive]: platform === 'react',
+        <div>
+            <div className="flex mb5">
+                <TextInput size="small" onChange={newValue => setFilter(newValue)} value={filter} />
+                <div className="ba b-gray br2 ml3 flex">
+                    <button
+                        type="button"
+                        onClick={() =>
+                            setCurrentPlatform(currentPlatform === 'react' ? null : 'react')
+                        }
+                        className={classNames({
+                            [styles.platformButton]: true,
+                            'br b-gray': true,
+                            [styles.isActive]: currentPlatform === 'react',
+                        })}
+                    >
+                        <Text size={2}>React</Text>
+                    </button>
+                    <button
+                        type="button"
+                        onClick={() => setCurrentPlatform(currentPlatform === 'ios' ? null : 'ios')}
+                        className={classNames({
+                            [styles.platformButton]: true,
+                            'br b-gray': true,
+                            [styles.isActive]: currentPlatform === 'ios',
+                        })}
+                    >
+                        <Text size={2}>iOS</Text>
+                    </button>
+                    <button
+                        type="button"
+                        onClick={() =>
+                            setCurrentPlatform(currentPlatform === 'android' ? null : 'android')
+                        }
+                        className={classNames({
+                            [styles.platformButton]: true,
+                            'br b-gray': true,
+                            [styles.isActive]: currentPlatform === 'android',
+                        })}
+                    >
+                        <Text size={2}>Android</Text>
+                    </button>
+                    <button
+                        type="button"
+                        onClick={() =>
+                            setCurrentPlatform(currentPlatform === 'scss' ? null : 'scss')
+                        }
+                        className={classNames({
+                            [styles.platformButton]: true,
+                            'br b-gray': true,
+                            [styles.isActive]: currentPlatform === 'scss',
+                        })}
+                    >
+                        <Text size={2}>SCSS</Text>
+                    </button>
+                    <button
+                        type="button"
+                        onClick={() =>
+                            setCurrentPlatform(currentPlatform === 'email' ? null : 'email')
+                        }
+                        className={classNames({
+                            [styles.platformButton]: true,
+                            [styles.isActive]: currentPlatform === 'email',
+                        })}
+                    >
+                        <Text size={2}>Email</Text>
+                    </button>
+                </div>
+            </div>
+
+            <div className={styles.componentGrid}>
+                {data.allCodaImplementationsTable.group
+                    .filter(component => {
+                        // If no platform filter is selected, all components should appear
+                        if (!currentPlatform) {
+                            return true;
+                        }
+
+                        // Show only the components for the selected platform that we've either
+                        // completed or know that we want to build.
+                        return component.edges.some(
+                            platform =>
+                                platform.node.data.values.Platform.toLowerCase() ===
+                                    currentPlatform.toLowerCase() &&
+                                [
+                                    'To-do',
+                                    'In progress',
+                                    'Done / Uses built in OS component',
+                                    'Done / Deprecated',
+                                    'Done / Merged',
+                                ].includes(platform.node.data.values.Development_status),
+                        );
+                    })
+                    .map(component => {
+                        console.log({ component });
+                        console.log({ data });
+
+                        const componentForCurrentPlatform = currentPlatform
+                            ? component.edges.find(
+                                  platform =>
+                                      platform.node.data.values.Platform.toLowerCase() ===
+                                      currentPlatform.toLowerCase(),
+                              ).node.data.values
+                            : null;
+
+                        console.log(componentForCurrentPlatform);
+
+                        const status = componentForCurrentPlatform
+                            ? componentForCurrentPlatform.Development_status
+                            : null;
+
+                        console.log(
+                            'usage',
+                            data.allCodaComponentsTable.edges.find(
+                                c => c.node.data.values.Usage_documentation,
+                            ),
+                        );
+
+                        let documentationLink;
+
+                        if (componentForCurrentPlatform) {
+                            // When viewing data for a specific platform, link to the documentation
+                            // for the component on that platform.
+                            documentationLink = componentForCurrentPlatform.Documentation || null;
+                        } else {
+                            // If no platform filter is selected, link to the component usage
+                            // documentation when available.
+                            const componentLevelData = data.allCodaComponentsTable.edges.find(
+                                c => c.node.data.values.Name === component.fieldValue,
+                            );
+
+                            documentationLink =
+                                componentLevelData.node.data.values.Usage_documentation || null;
+                        }
+
+                        const children = (
+                            <>
+                                <div className="h5 bg-gray-300 br1 mb1">&nbsp;</div>
+                                {status && (
+                                    <Text
+                                        size={3}
+                                        className="absolute top-0 right-0 bg-blue b pv1 ph3 white"
+                                    >
+                                        {status}
+                                    </Text>
+                                )}
+                                <Text size={2}>{component.fieldValue}</Text>
+                            </>
+                        );
+
+                        // Some components won't have a page that we can link to. This conditional
+                        // handles both cases.
+                        return documentationLink ? (
+                            <Link
+                                to={documentationLink.replace('https://thumbprint.design', '')}
+                                className="color-inherit relative"
+                                key={component.fieldValue}
+                            >
+                                {children}
+                            </Link>
+                        ) : (
+                            <div className="color-inherit relative" key={component.fieldValue}>
+                                {children}
+                            </div>
+                        );
                     })}
-                >
-                    <Text size={2}>React</Text>
-                </button>
-                <button
-                    type="button"
-                    onClick={() => setPlatform(platform === 'ios' ? undefined : 'ios')}
-                    className={classNames({
-                        [styles.platformButton]: true,
-                        'br b-gray': true,
-                        [styles.isActive]: platform === 'ios',
-                    })}
-                >
-                    <Text size={2}>iOS</Text>
-                </button>
-                <button
-                    type="button"
-                    onClick={() => setPlatform(platform === 'android' ? undefined : 'android')}
-                    className={classNames({
-                        [styles.platformButton]: true,
-                        'br b-gray': true,
-                        [styles.isActive]: platform === 'android',
-                    })}
-                >
-                    <Text size={2}>Android</Text>
-                </button>
-                <button
-                    type="button"
-                    onClick={() => setPlatform(platform === 'scss' ? undefined : 'scss')}
-                    className={classNames({
-                        [styles.platformButton]: true,
-                        'br b-gray': true,
-                        [styles.isActive]: platform === 'scss',
-                    })}
-                >
-                    SCSS
-                </button>
-                <button
-                    type="button"
-                    onClick={() => setPlatform(platform === 'email' ? undefined : 'email')}
-                    className={classNames({
-                        [styles.platformButton]: true,
-                        [styles.isActive]: platform === 'email',
-                    })}
-                >
-                    <Text size={2}>Email</Text>
-                </button>
             </div>
         </div>
     );
-};
+}
 
-export const ComponentRow = ({ name, react, scss, ios, android }) => (
-    <a href="/path/" className={`color-inherit relative ${styles.component}`}>
-        <div className="h5 bg-gray-300 br1 mb1">&nbsp;</div>
-        <Text size={3} className="absolute top-0 right-0 bg-blue b pv1 ph3 white">
-            {react.documentation}
-        </Text>
-        <Text size={2}>{name}</Text>
-    </a>
-);
-
-ComponentRow.propTypes = {
-    name: PropTypes.string.isRequired,
-    usage: PropTypes.shape({
-        design: documentationStatus.isRequired,
-        development: developmentStatus.isRequired,
-        documentation: documentationStatus.isRequired,
-    }).isRequired,
-    react: PropTypes.shape({
-        design: documentationStatus.isRequired,
-        development: developmentStatus.isRequired,
-        documentation: documentationStatus.isRequired,
-    }).isRequired,
-    scss: PropTypes.shape({
-        design: documentationStatus.isRequired,
-        development: developmentStatus.isRequired,
-        documentation: documentationStatus.isRequired,
-    }).isRequired,
-    ios: PropTypes.shape({
-        design: documentationStatus.isRequired,
-        development: developmentStatus.isRequired,
-        documentation: documentationStatus.isRequired,
-    }).isRequired,
-    android: PropTypes.shape({
-        design: documentationStatus.isRequired,
-        development: developmentStatus.isRequired,
-        documentation: documentationStatus.isRequired,
-    }).isRequired,
-};
-
-export const ComponentTable = ({ children }) => (
-    <div className={styles.componentGrid}>{children}</div>
-);
-
-ComponentTable.propTypes = {
-    children: PropTypes.node.isRequired,
-};
+// Component.propTypes = {
+//     name: PropTypes.string.isRequired,
+//     usage: PropTypes.shape({
+//         design: documentationStatus.isRequired,
+//         development: developmentStatus.isRequired,
+//         documentation: documentationStatus.isRequired,
+//     }).isRequired,
+//     react: PropTypes.shape({
+//         design: documentationStatus.isRequired,
+//         development: developmentStatus.isRequired,
+//         documentation: documentationStatus.isRequired,
+//     }).isRequired,
+//     scss: PropTypes.shape({
+//         design: documentationStatus.isRequired,
+//         development: developmentStatus.isRequired,
+//         documentation: documentationStatus.isRequired,
+//     }).isRequired,
+//     ios: PropTypes.shape({
+//         design: documentationStatus.isRequired,
+//         development: developmentStatus.isRequired,
+//         documentation: documentationStatus.isRequired,
+//     }).isRequired,
+//     android: PropTypes.shape({
+//         design: documentationStatus.isRequired,
+//         development: developmentStatus.isRequired,
+//         documentation: documentationStatus.isRequired,
+//     }).isRequired,
+// };
