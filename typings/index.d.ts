bc67df1c567e1493e4b6a243f144558ac301d731
Giles Lavelle <giles.lavelle@gmail.com>
2020-03-19 16:18:15 +0000

    Convert MDX renderer to TypeScript

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -5,13 +5,22 @@ declare module 'object-fit-images';
 declare module '@thumbtack/thumbprint-icons';
 
 declare module 'rotate-array' {
-    function rotate<T>(array: Array<T>, offset: number): Array<T>;
-    export default rotate;
+    export default function rotate<T>(array: Array<T>, offset: number): Array<T>;
 }
 
-type CSSModuleType = { [key: string]: string };
+declare module 'react-scroll-marker' {
+    export function ScrollMarkerSection({
+        id,
+        children,
+    }: {
+        id: string;
+        children: ({ id }: { id: string }) => JSX.Element;
+    }): JSX.Element;
+}
+
+declare module 'gatsby-plugin-mdx/mdx-renderer';
 
 declare module '*.module.scss' {
-    const classes: CSSModuleType;
+    const classes: { [key: string]: string };
     export default classes;
 }
