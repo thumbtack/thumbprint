63a926e387f7b8d37564cb70ab81077e2d040dbe
Daniel O'Connor <doconnor@thumbtack.com>
2020-06-16 14:24:54 -0700

    Remove code accidentally added in two places.

--- packages/thumbprint-react/components/Image/test.tsx
+++ packages/thumbprint-react/components/Image/test.tsx
@@ -2,44 +2,6 @@ import React from 'react';
 import { mount } from 'enzyme';
 import Image from './index';
 
-const observerMap = new Map();
-const instanceMap = new Map();
-
-beforeAll(() => {
-    // Mock `window.IntersectionObserver` using code from these two places:
-    // https://github.com/thebuilder/react-intersection-observer/blob/e31086c713615f3cfbe60eaa13491adcee3d41c2/src/test-utils.ts#L83
-    // https://jestjs.io/docs/en/manual-mocks#mocking-methods-which-are-not-implemented-in-jsdom
-    window.IntersectionObserver = {
-        writable: true,
-        value: jest.fn((cb, options) => {
-            const instance = {
-                thresholds: Array.isArray(options.threshold)
-                    ? options.threshold
-                    : [options.threshold],
-                root: options.root,
-                rootMargin: options.rootMargin,
-                time: Date.now(),
-                observe: jest.fn(element => {
-                    instanceMap.set(element, instance);
-                    observerMap.set(element, cb);
-                }),
-                unobserve: jest.fn(element => {
-                    instanceMap.delete(element);
-                    observerMap.delete(element);
-                }),
-                disconnect: jest.fn(),
-            };
-            return instance;
-        }),
-    };
-});
-
-afterAll(() => {
-    window.IntersectionObserver.mockClear();
-    instanceMap.clear();
-    observerMap.clear();
-});
-
 test('puts `alt` on the `img` tag', () => {
     const wrapper = mount(
         <Image
