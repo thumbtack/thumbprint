c0e31da65006bc0d4bff8b7e3d85864b121b7f8d
Giles Lavelle <giles.lavelle@gmail.com>
2020-07-16 19:55:16 +0100

    Upgrade Typescript and improve build scripts (#704)
    - Upgrade TypeScript to version 3.9.6. This gives access to null
      coalescing operator
    
    - Remove the custom prettier check script and just `prettier --check`
    
    - Run `yarn install` as part of `yarn bootstrap`. This ensures that
      `yarn start` and `yarn test` will never try to run with outdated
      dependencies (which can cause the build or tests to fail).

--- scripts/test-prettier.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-yarn prettier "**/*.{ts,tsx,js,jsx,css,scss,json,md,mdx,html}" --list-different || (c=$?; echo "
-⚠️  Some files in this branch have not been formatted with Prettier. Please run 'yarn run format' to format them.
-"; (exit $c))
