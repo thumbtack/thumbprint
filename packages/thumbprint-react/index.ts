f8a897e5b5ecd88290f94d8bea0bc57c34b4841f
Andre Gou <andregou@thumbtack.com>
2020-12-15 16:41:56 -0800

    Export types with export type syntax

--- packages/thumbprint-react/index.ts
+++ packages/thumbprint-react/index.ts
@@ -1,22 +1,42 @@
-export { UserAvatar, EntityAvatar } from './components/Avatar/index';
+export { UserAvatar, EntityAvatar} from './components/Avatar/index'
+export type {UserPropTypes as UserAvatarPropTypes, EntityPropTypes as EntityAvatarPropTypes } from './components/Avatar/index';
 export { default as Button, TextButton } from './components/Button/index';
+export type {ButtonPropTypes, TextButtonPropTypes} from './components/Button/index';
 export { BannerAlert, InPageAlert } from './components/Alert/index';
-export { default as AlertBanner } from './components/AlertBanner/index';
-export { default as ButtonRow } from './components/ButtonRow/index';
-export { default as Calendar } from './components/Calendar/index';
-export { default as Carousel } from './components/Carousel/index';
-export { default as Checkbox } from './components/Checkbox/index';
-export { default as Dropdown } from './components/Dropdown/index';
-export { TextFab, IconFab } from './components/Fab/index';
-export { default as FormNote } from './components/FormNote/index';
-export { default as HorizontalRule } from './components/HorizontalRule/index';
-export { Grid, GridColumn } from './components/Grid/index';
-export { default as Image } from './components/Image/index';
-export { default as InputRow, InputRowContext } from './components/InputRow/index';
-export { default as Label } from './components/Label/index';
-export { default as Link, ThemedLink } from './components/Link/index';
-export { List, ListItem } from './components/List/index';
-export { default as LoaderDots } from './components/LoaderDots/index';
+export type { BannerPropTypes, InPagePropTypes } from './components/Alert/index';
+export { default as AlertBanner} from './components/AlertBanner/index';
+export type {  AlertBannerPropTypes } from './components/AlertBanner/index';
+export { default as ButtonRow,} from './components/ButtonRow/index';
+export type { PropTypes as ButtonRowPropTypes } from './components/ButtonRow/index';
+export { default as Calendar} from './components/Calendar/index';
+export type {  PropTypes as CalendarPropTypes} from './components/Calendar/index';
+export { default as Carousel} from './components/Carousel/index';
+export type { PropTypes as CarouselPropTypes } from './components/Carousel/index';
+export { default as Checkbox} from './components/Checkbox/index';
+export type {  PropTypes as CheckBoxPropTypes } from './components/Checkbox/index';
+export { default as Dropdown} from './components/Dropdown/index';
+export type {  PropTypes as DropdownPropTypes } from './components/Dropdown/index';
+export { TextFab, IconFab, } from './components/Fab/index';
+export type { TextFabProps as TextFabPropTypes, IconFabProps as IconFabPropTypes } from './components/Fab/index';
+export { default as FormNote,} from './components/FormNote/index';
+export type { PropTypes as FormNotePropTypes } from './components/FormNote/index';
+export { default as HorizontalRule} from './components/HorizontalRule/index';
+export type { PropTypes as HorizontalRulePropTypes } from './components/HorizontalRule/index';
+export { Grid, GridColumn} from './components/Grid/index';
+export type {GridPropTypes, ColumnPropTypes as GridColumnPropTypes } from './components/Grid/index';
+export { default as Image} from './components/Image/index';
+export type {ImagePropTypes } from './components/Image/index';
+export { default as InputRow, InputRowContext} from './components/InputRow/index';
+export type {PropTypes as InputRowPropTypes } from './components/InputRow/index';
+export { default as Label} from './components/Label/index';
+export type {PropTypes as LabelPropTypes } from './components/Label/index';
+export { default as Link, ThemedLink} from './components/Link/index';
+export type {LinkPropTypes, ThemedLinkPropTypes } from './components/Link/index';
+export { List, ListItem} from './components/List/index';
+export type {ListPropTypes, ListItemPropTypes } from './components/List/index';
+export { default as LoaderDots} from './components/LoaderDots/index';
+export type {PropTypes as LoaderDotsPropTypes } from './components/LoaderDots/index';
+
 export {
     default as Modal,
     ModalHeader,
@@ -27,8 +47,19 @@ export {
     ModalFooter,
     ModalAnimatedWrapper,
 } from './components/Modal/index';
+export type {
+    ModalPropTypes,
+    ModalHeaderPropTypes,
+    ModalTitlePropTypes,
+    ModalDescriptionPropTypes,
+    ModalContentPropTypes,
+    ModalContentFullBleedPropTypes,
+    ModalFooterPropTypes,
+    ModalAnimatedWrapperPropTypes,
+} from './components/Modal/index';
 export { default as ModalBase } from './components/ModalBase/index.jsx';
-export { default as ModalCurtain } from './components/ModalCurtain/index';
+export { default as ModalCurtain} from './components/ModalCurtain/index';
+export type { PropTypes as ModalCurtainPropTypes } from './components/ModalCurtain/index';
 export { default as ModalStandard } from './components/ModalStandard/index.jsx';
 export {
     default as Popover,
@@ -37,21 +68,46 @@ export {
     PopoverPrimaryButton,
     PopoverSecondaryButton,
 } from './components/Popover/index';
+export type {
+    PopoverPropTypes,
+    PopoverTitlePropTypes,
+    PopoverBodyPropTypes,
+    PopoverPrimaryButtonPropTypes,
+    PopoverSecondaryButtonPropTypes
+} from './components/Popover/index';
 export { default as Pill } from './components/Pill/index';
-export { default as Radio } from './components/Radio/index';
+export type { PropTypes as PillPropTypes } from './components/Pill/index';
+export { default as Radio} from './components/Radio/index';
+export type {PropTypes as RadioPropTypes } from './components/Radio/index';
 export {
     default as ServiceCard,
     ServiceCardDescription,
     ServiceCardTitle,
     ServiceCardImage,
 } from './components/ServiceCard/index';
-export { default as StarRating } from './components/StarRating/index';
-export { default as TextArea } from './components/TextArea/index';
+export type {
+    ServiceCardPropTypes,
+    ServiceCardDescriptionPropTypes,
+    ServiceCardTitlePropTypes,
+    ServiceCardImagePropTypes,
+} from './components/ServiceCard/index';
+export { default as StarRating} from './components/StarRating/index';
+export type { PropTypes as StarRatingPropTypes } from './components/StarRating/index';
+export { default as TextArea} from './components/TextArea/index';
+export type { PropTypes as TextAreaPropTypes } from './components/TextArea/index';
 export {
     default as TextInput,
     TextInputIcon,
     TextInputClearButton,
 } from './components/TextInput/index';
-export { default as Tooltip } from './components/Tooltip/index';
-export { Title, Text } from './components/Type/index';
-export { default as Wrap } from './components/Wrap/index';
+export type {
+    TextInputPropTypes,
+    TextInputIconPropTypes,
+    TextInputClearButtonPropTypes,
+} from './components/TextInput/index';
+export { default as Tooltip} from './components/Tooltip/index';
+export type { TooltipPropTypes } from './components/Tooltip/index';
+export { Title, Text} from './components/Type/index';
+export type { TitlePropTypes, TextPropTypes } from './components/Type/index';
+export { default as Wrap} from './components/Wrap/index';
+export type { PropTypes as WrapPropTypes } from './components/Wrap/index';
