d95c2e143ff3bc2478b9aeec773e7f54de03f1af
Daniel O’Connor <doconnor@thumbtack.com>
2022-04-06 14:41:54 -0400

    Implement support for new `fetchPriority` feature in Chrome (#803)
    This will allow us to tell Chrome to fetch some `<Image>` components with higher priority. We'll do this by utilizing the `forceEarlyRender` prop which already existed to opt-out of lazy-loading and dynamic calculation of `sizes`.

--- packages/thumbprint-react/components/Image/index.tsx
+++ packages/thumbprint-react/components/Image/index.tsx
@@ -289,6 +289,10 @@ const Image = forwardRef<HTMLElement, ImageProps>((props: ImageProps, outerRef) 
                     onError={(): void => {
                         setIsError(true);
                     }}
+                    // We expect an error because the attribute is non-standard and doesn't yet
+                    // exist in the React types.
+                    // @ts-expect-error
+                    fetchPriority={forceEarlyRender ? 'high' : 'auto'}
                     className={classNames({
                         // Opacity to 0, prevents flash of alt text when `height` prop used
                         [styles.imageStart]: true,
