641cf73fc5f4b8b4edbe1eb94edc4d2be618000f
Giles Lavelle <giles.lavelle@gmail.com>
2022-08-10 13:39:52 +0100

    Add support for InputRow to Dropdown

--- packages/thumbprint-react/components/Dropdown/index.tsx
+++ packages/thumbprint-react/components/Dropdown/index.tsx
@@ -1,7 +1,8 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import * as tokens from '@thumbtack/thumbprint-tokens';
 import classNames from 'classnames';
 import styles from './index.module.scss';
+import { InputRowContext } from '../InputRow';
 
 const iconColor = {
     disabled: tokens.tpColorGray,
@@ -124,6 +125,10 @@ export default function Dropdown<T extends string | number = string>({
 }: DropdownProps<T>): JSX.Element {
     const uiState = getUIState({ isDisabled, hasError });
 
+    const { isFirstInputRowChild, isWithinInputRow, isLastInputRowChild } = useContext(
+        InputRowContext,
+    );
+
     return (
         <div
             className={classNames({
@@ -139,6 +144,10 @@ export default function Dropdown<T extends string | number = string>({
                     [styles.selectStateDefault]: uiState === 'default',
                     [styles.selectSizeSmall]: size === 'small',
                     [styles.selectSizeLarge]: size === 'large',
+                    // Styles for when the Dropdown is inside an InputRow
+                    [styles.selectRoundedBordersLeft]: isFirstInputRowChild || !isWithinInputRow,
+                    [styles.selectRoundedBordersRight]: isLastInputRowChild || !isWithinInputRow,
+                    [styles.selectHasNoRightBorder]: isWithinInputRow && !isLastInputRowChild,
                 })}
                 id={id}
                 disabled={isDisabled}
