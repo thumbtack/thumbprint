ee66580591fd42a67f0b92f8f5196ff03b864b80
Erik Goens <egoens@thumbtack.com>
2022-12-07 12:03:43 -0800

    Adds pill usage documentation (#858)
    * adds pill usage documentation
    
    * adjusts resolution on dos and donts images
    adjusts max sizing for dos and donts images
    
    * adds components for usage documentation
    adds additional usage documentation for pill component
    
    * fixes import order error

---
+++ www/src/components/anatomy/index.tsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import { Text } from '@thumbtack/thumbprint-react';
+import Badge from '../badge';
+import styles from './index.module.scss';
+
+interface PropTypes {
+    children: React.ReactNode;
+}
+
+interface AnatomyItemPropTypes {
+    children: React.ReactNode;
+    title: string;
+    number: string;
+}
+
+export function Anatomy({ children }: PropTypes): JSX.Element {
+    return <ul className={`flex-row  ${styles.children}`}>{children}</ul>;
+}
+
+export function AnatomyItem({ title, number, children }: AnatomyItemPropTypes): JSX.Element {
+    return (
+        <li className="flex pb3">
+            <div className="flex-column">
+                <Badge size={2}>{number}</Badge>
+            </div>
+            <div className="flex-column pl2">
+                <div className="flex-row">
+                    <Text size={1} className="b">
+                        {title}
+                    </Text>
+                </div>
+                <div className="flex-row">{children}</div>
+            </div>
+        </li>
+    );
+}
+
+export default { Anatomy };
