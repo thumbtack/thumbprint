2ad13d1f0ae37c15843db2af5984109ae3b17822
Daniel O'Connor <doconnor@thumbtack.com>
2023-01-10 15:04:46 -0500

    Create Tokens pages for all four platforms
    The sidenav still needs to be updated to include the tokens sections
    dynamically, but that will come later.

---
+++ next/components/thumbprint-tokens/token-section/token-example/token-example.tsx
@@ -0,0 +1,50 @@
+import React from 'react';
+import className from 'classnames';
+import { InlineCode } from '../../../mdx/mdx';
+
+interface TokenExampleProps {
+    exampleData: string;
+    displayText: string;
+    format: string | null;
+}
+
+export default function TokenExample({
+    exampleData,
+    displayText,
+    format,
+}: TokenExampleProps): JSX.Element {
+    const shouldRenderAsTwoLines = displayText.length > 30;
+    let children;
+
+    if (exampleData && format === 'color') {
+        children = (
+            <table className="w-100">
+                <tbody>
+                    <tr>
+                        <td
+                            className={className({
+                                flex: true,
+                                'tr items-center justify-end': !shouldRenderAsTwoLines,
+                                'flex-column items-end black-300': shouldRenderAsTwoLines,
+                            })}
+                        >
+                            <div
+                                className={className({
+                                    'h2 w4 ba b-gray-300': true,
+                                    mr4: !shouldRenderAsTwoLines,
+                                    mb1: shouldRenderAsTwoLines,
+                                })}
+                                style={{ backgroundColor: exampleData }}
+                            />
+                            <InlineCode theme="plain">{displayText}</InlineCode>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+        );
+    } else {
+        children = <InlineCode theme="plain">{displayText}</InlineCode>;
+    }
+
+    return <div>{children}</div>;
+}
